7022bdfdef61c1b4f805cb076c7969c50a3d9f03
Djalal Harouni <tixxdz@gmail.com>
2021-09-20 16:08:57 +0200

    bpf: add bpf_defs.h
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

---
+++ src/bpf_defs.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+/*
+ * Copyright (C) 2021 Djalal Harouni
+ */
+
+#ifndef __BPF_DEFS_H
+#define __BPF_DEFS_H
+
+struct bl_stat {
+	unsigned long  st_dev;	/* Device.  */
+	unsigned long  st_ino;	/* File serial number.  */
+};
+
+#endif /* __BPF_DEFS_H */
