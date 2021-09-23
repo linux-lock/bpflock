fb9adbb03e50a36253c34581fd56311e8e7eea48
Djalal Harouni <tixxdz@gmail.com>
2021-09-23 15:48:19 +0200

    bpf: add PROC_DYNAMIC_FIRST for ns usage
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

--- src/bpflock_bpf_defs.h
+++ src/bpflock_bpf_defs.h
@@ -7,6 +7,8 @@
 #ifndef __BPFLOCK_BPF_DEFS_H
 #define __BPFLOCK_BPF_DEFS_H
 
+#define PROC_DYNAMIC_FIRST 0xF0000000U
+
 struct bl_stat {
 	unsigned long  st_dev;	/* Device.  */
 	unsigned long  st_ino;	/* File serial number.  */
