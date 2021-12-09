2e62f73e174e11ba4b87d61aaa35e62756a07d74
Djalal Harouni <tixxdz@gmail.com>
2021-12-09 14:09:11 +0100

    test: add constant helper source
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

---
+++ test/helpers/cons.go
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2021 Djalal Harouni
+// Copyright 2017-2020 Authors of Cilium
+
+package helpers
+
+import (
+	"os"
+	"time"
+)
+
+var (
+	// HelperTimeout is a predefined timeout value for commands.
+	HelperTimeout = 4 * time.Minute
+
+	// ShortCommandTimeout is a timeout for commands which should not take a
+	// long time to execute.
+	ShortCommandTimeout = 10 * time.Second
+
+	// MidCommandTimeout is a timeout for commands which may take a bit longer
+	// than ShortCommandTimeout, but less time than HelperTimeout to execute.
+	MidCommandTimeout = 30 * time.Second
+)
+
+const (
+
+	//BpflockTestPath is the path where bpflock test code is located.
+	BpflockTestPath = "/src/github.com/linux-lock/bpflock/test"
+
+	TestResultsPath = "test_results/"
+	RunDir          = "/var/run/bpflock"
+	LibDir          = "/var/lib/bpflock"
+
+	LogPerm = os.FileMode(0666)
+)
