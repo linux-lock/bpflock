9c78aec86e51f70319d5c2c610a2565094f9e06b
Djalal Harouni <tixxdz@gmail.com>
2022-01-11 16:30:59 +0100

    daemon: create bpf programs and remove dead code
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

--- pkg/daemon/daemon.go
+++ pkg/daemon/daemon.go
@@ -63,7 +63,8 @@ func (d *Daemon) init() error {
 	// Remove any old bpf programs
 	bpf.BpfLsmDisable()
 
-	return nil
+	// Start all bpf programs again
+	return bpf.BpfLsmEnable()
 }
 
 // NewDaemon creates and returns a new Daemon with the parameters set in c.
@@ -79,8 +80,6 @@ func NewDaemon(ctx context.Context, cancel context.CancelFunc) (*Daemon, error) 
 		return nil, fmt.Errorf("invalid daemon configuration: %s", err)
 	}
 
-	/* TODO Load configuration and bpf related options */
-
 	err = bpf.ConfigureResourceLimits()
 	if err != nil {
 		log.WithError(err).Fatal("Unable to set memory resource limits")
@@ -94,15 +93,10 @@ func NewDaemon(ctx context.Context, cancel context.CancelFunc) (*Daemon, error) 
 	d.configModifyQueue = eventqueue.NewEventQueueBuffered("config-modify-queue", ConfigModifyQueueSize)
 	d.configModifyQueue.Run()
 
-	// Delete BPF programs on exit if running in tandem with Flannel.
-	/*if option.Config.FlannelUninstallOnExit {
-		cleaner.cleanupFuncs.Add(func() {
-			for _, ep := range d.endpointManager.GetEndpoints() {
-				ep.DeleteBPFProgramLocked()
-			}
-		})
+	err = d.init()
+	if err != nil {
+		return nil, fmt.Errorf("error while initializing daemon: %w", err)
 	}
-	*/
 
 	return &d, nil
 }
@@ -110,30 +104,6 @@ func NewDaemon(ctx context.Context, cancel context.CancelFunc) (*Daemon, error) 
 // Close shuts down a daemon
 func (d *Daemon) Close() {
 }
-
-// TriggerReloadWithoutCompile causes all BPF programs and maps to be reloaded,
-// without recompiling the datapath logic for each endpoint. It first attempts
-// to recompile the base programs, and if this fails returns an error. If base
-// program load is successful, it subsequently triggers regeneration of all
-// endpoints and returns a waitgroup that may be used by the caller to wait for
-// all endpoint regeneration to complete.
-//
-// If an error is returned, then no regeneration was successful. If no error
-// is returned, then the base programs were successfully regenerated, but
-// endpoints may or may not have successfully regenerated.
-/*func (d *Daemon) TriggerReloadWithoutCompile(reason string) (*sync.WaitGroup, error) {
-	log.Debugf("BPF reload triggered from %s", reason)
-	if err := d.Datapath().Loader().Reinitialize(d.ctx, d, d.mtuConfig.GetDeviceMTU(), d.Datapath(), d.l7Proxy); err != nil {
-		return nil, fmt.Errorf("Unable to recompile base programs from %s: %s", reason, err)
-	}
-
-	regenRequest := &regeneration.ExternalRegenerationMetadata{
-		Reason:            reason,
-		RegenerationLevel: regeneration.RegenerateWithDatapathLoad,
-	}
-	return d.endpointManager.RegenerateAllEndpoints(regenRequest), nil
-}
-*/
 
 func changedOption(key string, value option.OptionSetting, data interface{}) {
 	d := data.(*Daemon)
