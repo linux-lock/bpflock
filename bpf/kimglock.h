4fac409c169400b214be729a8fd3797eae66c50a
Djalal Harouni <tixxdz@gmail.com>
2022-01-17 18:18:02 +0100

    kimglock: update header file and include all enums
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

--- bpf/kimglock.h
+++ bpf/kimglock.h
@@ -4,8 +4,8 @@
  * Copyright (C) 2021 Djalal Harouni
  */
 
-#ifndef __BPFLOCK_KIMGBAN_H
-#define __BPFLOCK_KIMGBAN_H
+#ifndef __BPFLOCK_KIMGLOCK_H
+#define __BPFLOCK_KIMGLOCK_H
 
 #include "bpflock_security_class.h"
 #include "bpflock_bpf_defs.h"
@@ -17,48 +17,57 @@
 #define BPFLOCK_KI_PERM        1
 #define BPFLOCK_KI_ALLOW_OP    256
 
-enum ki_reason {
-        kireason_allow                  = 1,    /* Allow */
-        kireason_baseline_exception,            /* Baseline but allow with exception */
-        kireason_baseline,                      /* Baseline */
-        kireason_restricted,                          /* Restricted */
-};
-
 enum bpflock_bpf_perm_flag {
         BPFLOCK_KI_ALLOW       = 1,
         BPFLOCK_KI_BASELINE,
         BPFLOCK_KI_RESTRICTED,
 };
 
-enum lock_kimg_reason {
-        LOCK_KIMG_NONE = 0,
-        LOCK_KIMG_MODULE_SIGNATURE = 1,
-        LOCK_KIMG_DEV_MEM = 2,
-        LOCK_KIMG_EFI_TEST = 3,
-        LOCK_KIMG_KEXEC = 4,
-        LOCK_KIMG_HIBERNATION = 5,
-        LOCK_KIMG_PCI_ACCESS = 6,
-        LOCK_KIMG_IOPORT = 7,
-        LOCK_KIMG_MSR = 8,
-        LOCK_KIMG_ACPI_TABLES = 9,
-        LOCK_KIMG_PCMCIA_CIS = 10,
-        LOCK_KIMG_TIOCSSERIAL = 11,
-        LOCK_KIMG_MODULE_PARAMETERS = 12,
-        LOCK_KIMG_MMIOTRACE = 13,
-        LOCK_KIMG_DEBUGFS = 14,
-        LOCK_KIMG_XMON_WR = 15,
-        LOCK_KIMG_BPF_WRITE_USER = 16,
-        LOCK_KIMG_INTEGRITY_MAX = 17,
+#define FOREACH_KIMGREASON(KIMGREASON) \
+        KIMGREASON(LOCK_KIMG_NONE)      \
+        KIMGREASON(LOCK_KIMG_MODULE_SIGNATURE)  \
+        KIMGREASON(LOCK_KIMG_DEV_MEM)   \
+        KIMGREASON(LOCK_KIMG_EFI_TEST)   \
+        KIMGREASON(LOCK_KIMG_KEXEC)   \
+        KIMGREASON(LOCK_KIMG_HIBERNATION)   \
+        KIMGREASON(LOCK_KIMG_PCI_ACCESS)   \
+        KIMGREASON(LOCK_KIMG_IOPORT)   \
+        KIMGREASON(LOCK_KIMG_MSR)   \
+        KIMGREASON(LOCK_KIMG_ACPI_TABLES)   \
+        KIMGREASON(LOCK_KIMG_PCMCIA_CIS)   \
+        KIMGREASON(LOCK_KIMG_TIOCSSERIAL)   \
+        KIMGREASON(LOCK_KIMG_MODULE_PARAMETERS)   \
+        KIMGREASON(LOCK_KIMG_MMIOTRACE)   \
+        KIMGREASON(LOCK_KIMG_DEBUGFS)   \
+        KIMGREASON(LOCK_KIMG_XMON_WR)   \
+        KIMGREASON(LOCK_KIMG_BPF_WRITE_USER)   \
+        KIMGREASON(LOCK_KIMG_INTEGRITY_MAX)   \
+        KIMGREASON(LOCK_KIMG_KCORE)   \
+        KIMGREASON(LOCK_KIMG_KPROBES)   \
+        KIMGREASON(LOCK_KIMG_BPF_READ_KERNEL)   \
+        KIMGREASON(LOCK_KIMG_PERF)   \
+        KIMGREASON(LOCK_KIMG_TRACEFS)   \
+        KIMGREASON(LOCK_KIMG_XMON_RW)   \
+        KIMGREASON(LOCK_KIMG_XFRM_SECRET)   \
+        KIMGREASON(LOCK_KIMG_CONFIDENTIALITY_MAX)   \
+
+#define GENERATE_ENUM(ENUM) ENUM,
+
+enum kimg_enum {
+        FOREACH_KIMGREASON(GENERATE_ENUM)
+};
+
+enum extra_kimg_enum {
         /*
           We support only to BPF_WRITE_USER for backward compatibility,
-          LOCKDOWN_KCORE protects kcore which is read-only anyway.
+          LOCK_KIMG_KCORE protects kcore which is read-only anyway.
         */
 
         LOCK_KIMG_BTF_LOAD = 512,
 };
 
 struct bpflock_class_map kimg_security_map = {
-        "kernel image lock",
+        "kernel image lockdown",
         "/sys/fs/bpf/bpflock/kimglock",
         { NULL },
         { 0 }
@@ -76,6 +85,4 @@ struct bpflock_class_prog_link bpf_prog_links[] = {
 };
 
 /* End of kimglock security class */
-
-
 #endif /* __BPFLOCK_KIMGLOCK_H */
