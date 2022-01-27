93413f772e987050192e21f3466212bc388263c9
Djalal Harouni <tixxdz@gmail.com>
2022-01-27 21:57:33 +0100

    bpf: add bpflock shared object ids
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

---
+++ bpf/bpflock_shared_object_ids.h
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: LGPL-2.1
+
+/*
+ * Copyright (C) 2022 Djalal Harouni
+ */
+
+/* Shared defs between bpf and userspace programs */
+
+#ifndef __BPFLOCK_SHARED_OBJECT_IDS_H
+#define __BPFLOCK_SHARED_OBJECT_IDS_H
+
+/* This is the bpflock object which the whole program ID */ 
+enum bpflock_object_id {
+        NONE_ID                      = 0,
+        BPFRESTRICT_ID,
+        KMODLOCK_ID,
+        KIMGLOCK_ID,
+        FILELESS_ID,
+};
+
+#endif /* __BPFLOCK_SHARED_OBJECT_IDS_H */
