0f4220e40bbb61bfeca5576b19552ad6293a523e
Djalal Harouni <tixxdz@gmail.com>
2022-01-29 17:59:39 +0100

    bpf: standarize bpfrestrict values
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

--- bpf/bpfrestrict.h
+++ bpf/bpfrestrict.h
@@ -7,19 +7,25 @@
 #ifndef __BPFLOCK_BPFRESTRICT_H
 #define __BPFLOCK_BPFRESTRICT_H
 
+#include "bpflock_shared_object_ids.h"
 #include "bpflock_security_class.h"
 
 /* bpf security class */
 
-#define BPFLOCK_BPF_PERM        1
-#define BPFLOCK_BPF_OP          2
+#define LOG_BPFRESTRICT "bpfrestrict"
 
+/* Bpfrestrict arguments */
+#define BPFRESTRICT_PROFILE     1
+#define BPFRESTRICT_ALLOW_OP    2
+#define BPFRESTRICT_BLOCK_OP    3
+#define BPFRESTRICT_MAPS_FILTER 4
+#define BPFRESTRICT_DEBUG       5
+
+/* bpfrestrict defined allowed/blocked operations */
 #define BPFLOCK_MAP_CREATE      (1 << 0)
 #define BPFLOCK_BTF_LOAD        (1 << 1)
 #define BPFLOCK_PROG_LOAD       (1 << 2)
 #define BPFLOCK_BPF_WRITE       (1 << 8)
-
-#define BPFRESTRICT_NS_MAP_PIN      "/sys/fs/bpf/bpflock/bpfrestrict_ns_map"
 
 struct bpflock_class_map bpf_security_map = {
         "bpf",
@@ -31,15 +37,13 @@ struct bpflock_class_map bpf_security_map = {
 struct bpflock_class_prog_link bpf_prog_links[] = {
         {
                 "bpflock_bpfrestrict",
-                "/sys/fs/bpf/bpflock/bpfrestrict/bpfrestrict_link",
+                "/sys/fs/bpf/bpflock/bpfrestrict/bpfrestrict_bpf_link",
         },
         {
                 "bpflock_bpfrestrict_bpf_write",
-                "/sys/fs/bpf/bpflock/bpfrestrict/bpfrestrict_bpf_write_link",
+                "/sys/fs/bpf/bpflock/bpfrestrict/bpfrestrict_locked_down_link",
         },
 };
 
 /* End of bpf security class */
-
-
 #endif /* __BPFLOCK_BPFRESTRICT_H */
