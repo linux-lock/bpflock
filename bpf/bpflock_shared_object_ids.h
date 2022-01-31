81e4e3bacab3a161d0dbd6587077bccc6aa57d7c
Djalal Harouni <tixxdz@gmail.com>
2022-01-31 13:13:50 +0100

    bpf: add events ids definition
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

--- bpf/bpflock_shared_object_ids.h
+++ bpf/bpflock_shared_object_ids.h
@@ -11,12 +11,38 @@
 
 /* This is the bpflock object which the whole program ID */ 
 enum bpflock_object_id {
-        NONE_ID                      = 0,
-        EXECSNOOP_ID,
+        EXECSNOOP_ID                    = 1,
         BPFRESTRICT_ID,
         KMODLOCK_ID,
         KIMGLOCK_ID,
         FILELESS_ID,
 };
+
+enum bpflock_event_id {
+        /*
+         * Syscall numbers:
+         * https://chromium.googlesource.com/chromiumos/docs/+/master/constants/syscalls.md
+         * Since they differ from one arch to another we just go with a small set of syscalls
+         * that we are interested in and use same values starting from 10000
+         */
+
+        /* Keep names descriptive */
+
+        /* Syscalls start from 10000 */
+        SYSCALL_EXECVE_ID               = 10000,
+        SYSCALL_EXECVEAT_ID,
+        SYSCALL_BPF_ID,
+
+        /* LSM IDs start from 20000 */
+
+        /* LSM BPF CONTEXT */
+        LSM_BPF_ID                      = 20000,
+        LSM_BPF_MAP_ID,
+
+        /* LSM LOCKED_DOWN CONTEXT */
+        LSM_LOCKED_DOWN_ID              = 20100,
+};
+
+#define event_id_to_str(id)         #id
 
 #endif /* __BPFLOCK_SHARED_OBJECT_IDS_H */
